f978c51cfa37e3056855dfb8b4b9fe580149c3a4
Julian Douglas <84514009+julian-douglas@users.noreply.github.com>
2021-08-29 14:05:48 +0100

    Update train.py

--- train.py
+++ train.py
@@ -51,9 +51,6 @@ LOCAL_RANK = int(os.getenv('LOCAL_RANK', -1))  # https://pytorch.org/docs/stable
 RANK = int(os.getenv('RANK', -1))
 WORLD_SIZE = int(os.getenv('WORLD_SIZE', 1))
 
-with open("models/modified_yolov5s.yaml", 'r') as stream:
-    modified = yaml.safe_load(stream)
-
 def train(hyp,  # path/to/hyp.yaml or hyp dictionary
           opt,
           device,
